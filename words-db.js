88b26467471321e37dae0329e93f889b9b3d87ed
Victor Miguel Vilela Fernandes <113549997+victormiguelVF@users.noreply.github.com>
2023-03-30 13:05:40 -0300

    Jogo de adivinhar a palavra com JS

---
+++ words-db.js
@@ -0,0 +1,334 @@
+let wordList = [
+    {
+        word: "banana",
+        hint: "fruta amarela"
+    },
+    {
+        word: "doce",
+        hint: "de comer, que não é salgado"
+    },
+    {
+        word: "github",
+        hint: "plataforma de hospedagem de código"
+    },
+    {
+        word: "png",
+        hint: "formato de arquivo de imagem"
+    },
+    {
+        word: "prata",
+        hint: "precioso metal branco acinzentado"
+    },
+    {
+        word: "celular",
+        hint: "um aparelho eletrônico"
+    },
+    {
+        word: "cpu",
+        hint: "parte de um computador"
+    },
+    {
+        word: "java",
+        hint: "linguagem de programação"
+    },
+    {
+        word: "google",
+        hint: "famoso site de pesquisa"
+    },
+    {
+        word: "veneza",
+        hint: "famosa cidade das águas"
+    },
+    {
+        word: "excel",
+        hint: "produto da microsoft para windows"
+    },
+    {
+        word: "dancar",
+        hint: "se mover em uma música"
+    },
+    {
+        word: "aguia",
+        hint: "ave de rapina"
+    },
+    {
+        word: "uva",
+        hint: "fruta pequena"
+    },
+    {
+        word: "mel",
+        hint: "xarope doce"
+    },
+    {
+        word: "geleia",
+        hint: "pasta de frutas"
+    },
+    {
+        word: "limao",
+        hint: "fruta cítriva azeda"
+    },
+    {
+        word: "melao",
+        hint: "fruta doce"
+    },
+    {
+        word: "oliva",
+        hint: "fruta pequena"
+    },
+    {
+        word: "pessego",
+        hint: "fruta macia"
+    },
+    {
+        word: "rainha",
+        hint: "comandante feminina"
+    },
+    {
+        word: "coelho",
+        hint: "animal rápido"
+    },
+    {
+        word: "aranha",
+        hint: "inseto de oito patas"
+    },
+    {
+        word: "tigre",
+        hint: "gato grande"
+    },
+    {
+        word: "tio",
+        hint: "irmão do seu pai"
+    },
+    {
+        word: "virus",
+        hint: "doente"
+    },
+    {
+        word: "agua",
+        hint: "líquido limpo"
+    },
+    {
+        word: "xenon",
+        hint: "elemento químico"
+    },
+    {
+        word: "iate",
+        hint: "bote"
+    },
+    {
+        word: "pao",
+        hint: "comida de padaria"
+    },
+    {
+        word: "queijo",
+        hint: "os ratos adoram"
+    },
+    {
+        word: "burro",
+        hint: "parente do cavalo"
+    },
+    {
+        word: "gemada",
+        hint: "ovos batidos"
+    },
+    {
+        word: "geladeira",
+        hint: "eletrodoméstico"
+    },
+    {
+        word: "guitarra",
+        hint: "instrumento musical"
+    },
+    {
+        word: "hockey",
+        hint: "esporte praticado no gelo"
+    },
+    {
+        word: "iglu",
+        hint: "casa no gelo"
+    },
+    {
+        word: "jaqueta",
+        hint: "roupa"
+    },
+    {
+        word: "chaleira",
+        hint: "esquenta a água"
+    },
+    {
+        word: "folhas",
+        hint: "parte da árvore"
+    },
+    {
+        word: "barata",
+        hint: "inseto"
+    },
+    {
+        word: "macarrao",
+        hint: "pasta"
+    },
+    {
+        word: "laranja",
+        hint: "fruta cítrica"
+    },
+    {
+        word: "pimenta",
+        hint: "picante"
+    },
+    {
+        word: "mysql",
+        hint: "um sistema de banco de dados relacional"
+    },
+    {
+        word: "iran",
+        hint: "país em desenvolvimento"
+    },
+    {
+        word: "flauta",
+        hint: "instrumento musical"
+    },
+    {
+        word: "crypto",
+        hint: "relacionado a cryptomoeda"
+    },
+    {
+        word: "tesla",
+        hint: "unidade de densidade de fluxo magnético"
+    },
+    {
+        word: "marte",
+        hint: "planeta do sistema solar"
+    },
+    {
+        word: "proxy",
+        hint: "relacionado a aplicação em servidores"
+    },
+    {
+        word: "email",
+        hint: "trocar mensagens online"
+    },
+    {
+        word: "html",
+        hint: "linguagem de marcação para web"
+    },
+    {
+        word: "ar",
+        hint: "relacionado a um gás"
+    },
+    {
+        word: "ideia",
+        hint: "um pensamento ou sugestão"
+    },
+    {
+        word: "server",
+        hint: "relacionado a um sistema de computador"
+    },
+    {
+        word: "svg",
+        hint: "um formato de imagem vetorial"
+    },
+    {
+        word: "jpeg",
+        hint: "um formato de imagem"
+    },
+    {
+        word: "procurar",
+        hint: "ato de achar alguma coisa"
+    },
+    {
+        word: "chave",
+        hint: "usado para abrir"
+    },
+    {
+        word: "egito",
+        hint: "país africano"
+    },
+    {
+        word: "coringa",
+        hint: "filme de terror psicolõgico"
+    },
+    {
+        word: "dubai",
+        hint: "nome de um país em desenvolvimento"
+    },
+    {
+        word: "foto",
+        hint: "representação de pessoa ou cena"
+    },
+    {
+        word: "nilo",
+        hint: "maior rio do mundo"
+    },
+    {
+        word: "chuva",
+        hint: "água que cai do céu"
+    },
+    {
+        word: "python",
+        hint: "linguagem de programação"
+    },
+    {
+        word: "mirar",
+        hint: "antes de atirar"
+    },
+    {
+        word: "venus",
+        hint: "planeta do sistema solar"
+    },
+    {
+        word: "ouro",
+        hint: "meteal precioso amarelo"
+    },
+    {
+        word: "shopee",
+        hint: "site de compras online"
+    },
+    {
+        word: "php",
+        hint: "linguagem de programação"
+    },
+    {
+        word: "codar",
+        hint: "relacionado a programar"
+    },
+    {
+        word: "matrix",
+        hint: "filme de ficção científica"
+    },
+    {
+        word: "bugs",
+        hint: "relacionado a programação"
+    },
+    {
+        word: "avatar",
+        hint: "filme épico de ficção científica"
+    },
+    {
+        word: "gif",
+        hint: "um formato de arquivo para imagem"
+    },
+    {
+        word: "mental",
+        hint: "relacionado a mente"
+    },
+    {
+        word: "mapa",
+        hint: "diagrama que representa uma área"
+    },
+    {
+        word: "ilha",
+        hint: "terra cercada por água"
+    },
+    {
+        word: "vingadores",
+        hint: "filme de heróis famoso"
+    },
+    {
+        word: "xadrez",
+        hint: "jogo de tabuleiro"
+    },
+    {
+        word: "instagram",
+        hint: "rede social"
+    },
+]
